aa593474a65711c00fab1fdf556c69cb11051e64
Nickson Santos Endlich <nickson12@gmail.com>
2022-06-25 18:57:29 -0300

    Fim da Aula 2 - Mais FileStream e leitor StreamReader - Alura - C# Parte 9: entrada e saída (I/O) com streams

---
+++ ByteBank.ImportacaoExportacao/1_LidandoComStreamDiretamente.cs
@@ -0,0 +1,73 @@
+﻿using ByteBank.ImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.ImportacaoExportacao
+{
+    partial class Program
+    {
+        static void LidandoComFileStreamDiretamente()
+        {
+            string arquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(arquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024];
+
+                fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+
+                var quantidadeBytesLidos = -1;
+
+                do
+                {
+                    quantidadeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer, quantidadeBytesLidos);
+                } while (quantidadeBytesLidos != 0);
+
+            }
+        }
+
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var encoding = new UTF8Encoding();
+            var texto = encoding.GetString(buffer, 0, bytesLidos);
+
+            Console.Write(texto);
+        }
+
+        static void TesteInicialLeituraDeArquivo()
+        {
+            Console.WriteLine("TesteInicialLeituraDeArquivo()\n");
+            string enderecoDoArquivo = "contas.txt";
+
+            List<ContaCorrente> listaContas = new List<ContaCorrente>();
+
+            try
+            {
+                IEnumerable<string> linhas = System.IO.File.ReadLines(enderecoDoArquivo);
+
+                foreach (string linha in linhas)
+                {
+                    string[] registros = linha.Split(' ');
+                    int agencia = Int32.Parse(registros[0]);
+                    int numero = Int32.Parse(registros[1]);
+                    double saldo = Double.Parse(registros[2]);
+                    string nomeDoTitular = registros[3];
+                    Cliente cliente = new Cliente(nomeDoTitular);
+
+                    listaContas.Add(new ContaCorrente(agencia, numero, saldo, cliente));
+                }
+                Console.WriteLine(listaContas.ExibeListaContas());
+                Console.WriteLine(ContaCorrente.TotalContaCorrente);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+    }
+}
--- ByteBank.ImportacaoExportacao/Program.cs
+++ ByteBank.ImportacaoExportacao/Program.cs
@@ -3,68 +3,28 @@ using System.Text;
 
 namespace ByteBank.ImportacaoExportacao
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
             //TesteInicialLeituraDeArquivo();
 
-            string arquivo = "contas.txt";
+            //LidandoComFileStreamDiretamente();
 
-            var fluxoDoArquivo = new FileStream(arquivo, FileMode.Open);
-
-            var buffer = new byte[1024];
-
-            fluxoDoArquivo.Read(buffer, 0, buffer.Length);
-
-            var quantidadeBytesLidos = -1;
-            while (quantidadeBytesLidos != 0)
-            {
-                quantidadeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
-            }
-
-        }
-
-        static void EscreverBuffer(byte[] buffer)
-        {
-            var encoding = new UTF8Encoding();
-            var texto = encoding.GetString(buffer);
-
-            Console.Write(texto);
-        }
-
-        static void TesteInicialLeituraDeArquivo()
-        {
-            Console.WriteLine("TesteInicialLeituraDeArquivo()\n");
             string enderecoDoArquivo = "contas.txt";
 
-            List<ContaCorrente> listaContas = new List<ContaCorrente>();
-
-            try
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDeArquivo))
             {
-                IEnumerable<string> linhas = System.IO.File.ReadLines(enderecoDoArquivo);
-
-                foreach (string linha in linhas)
+                while (!leitor.EndOfStream)
                 {
-                    string[] registros = linha.Split(' ');
-                    int agencia = Int32.Parse(registros[0]);
-                    int numero = Int32.Parse(registros[1]);
-                    double saldo = Double.Parse(registros[2]);
-                    string nomeDoTitular = registros[3];
-                    Cliente cliente = new Cliente(nomeDoTitular);
-
-                    listaContas.Add(new ContaCorrente(agencia, numero, saldo, cliente));
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
                 }
-                Console.WriteLine(listaContas.ExibeListaContas());
-                Console.WriteLine(ContaCorrente.TotalContaCorrente);
             }
-            catch(Exception ex)
-            {
-                throw;
-            }
-                
         }
+
+        
 
     }
 }
